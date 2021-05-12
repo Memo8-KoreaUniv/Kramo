00647d19f8c656c301e7cc0c1c787cfafaf28e46
vwjdtjdwnsv <vwjdtjdwnsv@gmail.com>
2021-05-12 20:53:08 +0900

    text 추가

---
+++ src/pages/textmemo.tsx
@@ -0,0 +1,17 @@
+
+import styled from 'styled-components'
+import React, { FC } from 'react';
+import { Button } from 'antd';
+
+const CustomDiv = styled.div`
+  font-size: 30px;
+`
+
+export default function Index() {
+  return (
+    <>
+      <CustomDiv>KUM (Korea Univ Memo)</CustomDiv>
+    </>
+  )
+}
+
