15b01427bf6bac0f15db108f85f74cb7f84264d3
SeoSang <ddrrpg@naver.com>
2021-05-15 01:16:30 +0900

    [mongo] User, Category 구성

--- server/DB/index.ts
+++ server/DB/index.ts
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const connectDB = async () => {
   let uri
-
-  switch (process.env.NODE_ENV.trim()) {
+  switch (process.env['NODE_ENV']?.trim()) {
     case 'dev':
-      uri = process.env.MONGODB_URI_DEV
+      uri = process.env['MONGODB_URI_DEV']
+      console.log(process.env)
+      console.log(process.env['MONGODB_URI_DEV'])
       break
     case 'pro':
-      uri = process.env.MONGODB_URI_PRO
+      uri = process.env['MONGODB_URI_PRO']
       break
     default:
       break
   }
   if (!uri) {
+    console.log(`MongoDB Connected Fail!`)
     return
   }
   const conn = await mongoose.connect(uri, {
--- server/DB/user.ts
+++ server/DB/user.ts
@@ -1,29 +1,50 @@
-import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import mongoose, { Schema } from 'mongoose'
+import { Document, Model, model, Schema, Date, Types } from 'mongoose'
+import { CategoryDocument } from './category'
 
-const UserSchema = new Schema({
-  id: mongoose.Schema.Types.ObjectId,
-  nickname: String,
+/* https://medium.com/@agentwhs/complete-guide-for-typescript-for-mongoose-for-node-js-8cc0a7e470c1 */
+// Schema
+const UserSchema = new Schema<UserDocument, UserModel>({
   email: {
     type: String,
-    required: true,
     unique: true,
+    required: true,
     lowercase: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
   },
   password: {
     type: String,
     required: true,
-    trim: true,
   },
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   created_at: {
     type: Date,
     default: Date.now,
   },
+  categories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
 })
 
-UserSchema.UserSchema.pre('save', async function (next) {
+export interface User {
+  email: string
+  nickname?: string
+  password: string
+  created_at: Date
+  categories?: Array<Types.ObjectId> | Array<Record<string, unknown>>
+}
+
+interface UserBaseDocument extends User, Document {
+  categories: Types.Array<CategoryDocument['_id']>
+}
+export interface UserDocument extends UserBaseDocument {}
+export interface UserModel extends Model<UserDocument> {}
+export interface UserPopulatedDocument extends UserBaseDocument {
+  categories: Types.Array<CategoryDocument>
+}
+
+// Document middlewares
+UserSchema.pre<UserDocument>('save', async function (next) {
   if (!this.isModified('password')) {
     next()
   }
@@ -32,14 +53,9 @@ UserSchema.UserSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt)
 })
 
-UserSchema.methods.getSignedJwtToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  })
-}
-
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
-export default mongoose.model('User', UserSchema)
+// Default export
+export default model<UserDocument, UserModel>('User', UserSchema)
