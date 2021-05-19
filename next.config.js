9802ad6676ff5dfce59bb3883895fed0722cebfd
SeoSang <ddrrpg@naver.com>
2021-05-19 19:31:55 +0900

    [Hotfix] nextjs file 라우팅 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {}, // add your env variables https://nextjs.org/docs/api-reference/next.config.js/environment-variables
   generateEtags: true,
-  useFileSystemPublicRoutes: false,
-  webpack: (config) => {
+  useFileSystemPublicRoutes: true,
+  webpack: config => {
     const originalEntry = config.entry
     config.entry = async () => {
       const entries = await originalEntry()
