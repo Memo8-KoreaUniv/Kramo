98c8afb440d5708030547783b0d7c6988dd91890
SeoSang <ddrrpg@naver.com>
2021-05-19 21:07:34 +0900

    [mongo] lint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ['eslint-config-yceffort/typescript'],
   rules: {
     '@typescript-eslint/naming-convention': customRule.extendedRules,
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     'react/react-in-jsx-scope': ['off'],
   },
 }
