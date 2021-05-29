40798b8e9f60eff2c2215f831d5aa28f220bad58
namwindows <namwindows@gmail.com>
2021-05-29 16:11:13 +0900

    카드 사이즈 반응형 조정

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import {
   Card,
   Avatar,
@@ -36,16 +36,29 @@ function useMemos() {
   }
 }
 
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
+
 export function Main() {
   const { memos, deleteMemo } = useMemos()
-
+  
   return (
     <div
       className="site-layout-background"
       style={{ padding: 24, textAlign: 'left' }}>
       <Row>
         <MemoView memos={memos} deleteMemo={deleteMemo} />
-        <MemoTimeline />
       </Row>
     </div>
   )
@@ -142,7 +155,8 @@ function MemoCardItem({ memo, deleteMemo }) {
 
   return (
     <Card
-      style={{ width: 300 }}
+      style={{ width: useWindowSize()[0] > 400 ? 300 : 270 }}
+      size={ useWindowSize()[0] > 400 ? "default" : "small"}
       actions={[
         <FolderOpenOutlined key="open" onClick={showDrawer} />,
         <Link href={{ pathname: '/editor' }}>
