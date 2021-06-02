59d9286b7d6367e0393b488791b25be1263e2a0e
SeoSang <ddrrpg@naver.com>
2021-06-02 16:26:34 +0900

    [api-doc] category 관련 api 구현

--- pages/api/category/[...param].ts
+++ pages/api/category/[...param].ts
@@ -7,26 +7,31 @@ import { connectToDatabase } from 'src/utils/mongo'
 import { pageParser } from 'src/utils/pageParser'
 
 /**
- * /api/user/{userId}/memos
+ * /api/category/{categoryId}/memos
  * @example req.query
  *   {
- *     "param": [`userId`, "memos"]
+ *     "param": [`categoryId`, "memos"]
  *     "page" : 1
  *     "count" : 9
  *   }
  *
  */
-export default async function user(req: NextApiRequest, res: NextApiResponse) {
+export default async function category(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const { param } = req.query
-  const userId = param[0]
+  const categoryId = param[0]
   try {
     await connectToDatabase()
     switch (param[1]) {
-      /* ​/api​/user​/{userId}​/memos */
+      /* ​/api​/user​/{categoryId}​/memos */
       case 'memos':
         if (req.method == 'GET') {
           const { page, count } = req.query
-          const histories = await HistoryModel.find({ user: userId as any })
+          const histories = await HistoryModel.find({
+            category: categoryId as any,
+          })
           const obj: { [key: string]: any } = {}
           for (const history of histories) {
             const memoId = history.memo as any
@@ -45,43 +50,7 @@ export default async function user(req: NextApiRequest, res: NextApiResponse) {
             page as string,
             count as string,
           )
-
           res.status(200).json({ memos: listMemoResult })
-        }
-        res.status(501).json({ alertText: 'Unexpected request Method!' })
-        break
-      case 'category':
-        if (req.method == 'POST') {
-          const { name } = req.query
-          try {
-            const addCategoryResult = await CategoryModel.create({
-              user: userId,
-              name,
-            })
-            res.status(200).json({ category: addCategoryResult })
-          } catch (e) {
-            console.log(e)
-            res.status(409).json({
-              alertText: '카테고리가 이미 존재하거나 DB에 오류가 생겼습니다!',
-            })
-          }
-        }
-        res.status(501).json({ alertText: 'Unexpected request Method!' })
-        break
-      case 'categories':
-        if (req.method == 'GET') {
-          const count = req.query.count as string
-          try {
-            const findCategoriesResult = await CategoryModel.find({
-              user: userId as any,
-            }).limit(parseInt(count))
-            res.status(200).json({ categories: findCategoriesResult })
-          } catch (e) {
-            console.log(e)
-            res.status(409).json({
-              alertText: '카테고리가 존재하지 않습니다!',
-            })
-          }
         }
         res.status(501).json({ alertText: 'Unexpected request Method!' })
         break
