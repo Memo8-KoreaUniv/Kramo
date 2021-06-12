61f21082775ff1ed78241fb3bf4901f7e7a070d5
SeoSang <ddrrpg@naver.com>
2021-06-12 23:53:39 +0900

    [admin] 관리자 로그인용 페이지 추가

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import { Button, Form, Input, Radio, Typography } from 'antd'
 
+import kaxios from 'src/interceptors'
 import { FlexDiv } from 'style/div'
 
 type SizeType = Parameters<typeof Form>[0]['size']
@@ -13,13 +14,32 @@ type SizeType = Parameters<typeof Form>[0]['size']
 const Register = () => {
   const [componentSize, setComponentSize] =
     useState<SizeType | 'default'>('default')
+
   const onFormLayoutChange = ({ size }: { size: SizeType }) => {
     setComponentSize(size)
   }
+
+  const onClickLogin = async (values: { email: string; password: string }) => {
+    try {
+      await kaxios({
+        url: '/admin/login',
+        data: values,
+        method: 'post',
+      })
+      alert('로그인 성공!')
+    } catch (e) {
+      alert(
+        e.response?.data?.alertText
+          ? e.response?.data?.alertText
+          : '로그인 실패!',
+      )
+    }
+  }
+
   return (
     <>
       <FlexDiv margin="1rem">
-        <Typography.Title>회원가입</Typography.Title>
+        <Typography.Title>관리자 로그인</Typography.Title>
       </FlexDiv>
       <Form
         labelCol={{ span: 4 }}
@@ -27,25 +47,25 @@ const Register = () => {
         layout="horizontal"
         initialValues={{ size: componentSize }}
         onValuesChange={onFormLayoutChange}
+        onFinish={onClickLogin}
         size={componentSize as SizeType}>
-        <Form.Item label="Form Size" name="size">
+        <Form.Item label="Form Size">
           <Radio.Group>
             <Radio.Button value="small">Small</Radio.Button>
             <Radio.Button value="default">Default</Radio.Button>
             <Radio.Button value="large">Large</Radio.Button>
           </Radio.Group>
         </Form.Item>
-        <Form.Item label="이메일">
+        <Form.Item label="이메일" name="email">
           <Input />
         </Form.Item>
-        <Form.Item label="이름">
-          <Input />
-        </Form.Item>
-        <Form.Item label="닉네임">
-          <Input />
+        <Form.Item label="관리자용 비밀번호" name="password">
+          <Input.Password />
         </Form.Item>
         <FlexDiv>
-          <Button>로그인</Button>
+          <Button type="primary" htmlType="submit">
+            로그인
+          </Button>
           <Button>가입하기</Button>
         </FlexDiv>
       </Form>
