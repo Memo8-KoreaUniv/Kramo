1aecc858bafab24b4e3dd45df9cce586af437003
Jeongjoo Lee <pyville@users.noreply.github.com>
2021-06-19 00:29:47 +0900

    [Frontend] 날짜/시간 관련 코드  마이너 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,22 +6,22 @@ export const formatDate = (inputDate: Date, now: Date): string => {
   const year =
     inputDate.getFullYear() === now.getFullYear()
       ? ''
-      : `${inputDate.getFullYear()}년`
+      : `${inputDate.getFullYear()}년 `
 
   const date = today
     ? ''
-    : `${inputDate.getMonth() + 1}월 ${inputDate.getDate()}일`
+    : `${inputDate.getMonth() + 1}월 ${inputDate.getDate()}일 `
 
   const time = today
     ? compareTime(inputDate, now)
     : `${inputDate.getHours()}시 ${inputDate.getMinutes()}분`
-  return `${year} ${date} ${time}`
+  return `${year}${date}${time}`
 }
 
 const compareTime = (a: Date, b: Date): string => {
   const hoursCollapsed: number = b.getHours() - a.getHours()
   const minutesCollapsed: number =
-    (b.getHours() - a.getHours()) * 60 + b.getMinutes() - a.getMinutes()
+    hoursCollapsed * 60 + b.getMinutes() - a.getMinutes()
   return minutesCollapsed >= 60
     ? `${hoursCollapsed}시간 전`
     : `${minutesCollapsed}분 전`
