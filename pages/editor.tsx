c4f204e5af6583f46db26ea68cc9a6494f8bb2f6
vwjdtjdwnsv <vwjdtjdwnsv@gmail.com>
2021-06-19 19:08:42 +0900

    메모 수정 시 GPS,weathre 반영

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,4 +1,4 @@
-import React, { LegacyRef, useEffect } from 'react'
+import React, { LegacyRef, useEffect, useState } from 'react'
 
 import { Editor as ToastUIEditor } from '@toast-ui/react-editor'
 import { message } from 'antd'
@@ -7,7 +7,16 @@ import dynamic from 'next/dynamic'
 import { useSetRecoilState } from 'recoil'
 
 import { historiesState, loadHistories, addHistories } from 'src/state/history'
+import { GPS } from 'src/types'
 import { HistoryInfo } from 'src/types/history'
+import { DEFAULT_GPS, getLocation } from 'src/utils/gps'
+import {
+  WeatherInfo,
+  getIconURL,
+  getNowWeatherByGeo,
+  EMPTY_WEATHER,
+} from 'src/utils/weather'
+
 const PostEditor = dynamic(() => import('src/components/ToastEditor'), {
   ssr: false,
 })
@@ -21,16 +30,39 @@ const Editor = ({ histories }: { histories: HistoryInfo[] }): JSX.Element => {
     setHistories(histories)
   }, [])
 
+  const [GPS, setGPS] = useState<GPS>(DEFAULT_GPS)
+  const [currentWeather, setCurrentWeather] =
+    useState<WeatherInfo>(EMPTY_WEATHER)
+
   const addMemo = async () => {
+    getLocation(navigator.geolocation, (pos: GeolocationPosition) => {
+      setGPS({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      })
+    })
+
+    getNowWeatherByGeo(
+      GPS.latitude,
+      GPS.longitude,
+      process.env.NEXT_PUBLIC_WEATHER_API_KEY!,
+    ).then((res) => {
+      setCurrentWeather(res)
+    })
+
     const innerText = editorRef.current?.getInstance().getHtml()
     if (innerText === undefined) {
       return message.error('아무것도 입력되지 않았습니다!')
     }
 
-    const newHistory: HistoryInfo = { ...histories[0], text: innerText }
+    const newHistory: HistoryInfo = {
+      ...histories[0],
+      text: innerText,
+      gps: GPS,
+      weather: currentWeather,
+    }
     delete newHistory._id
     delete newHistory.createdAt
-
     addHistories(newHistory)
   }
 
